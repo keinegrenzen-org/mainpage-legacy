a470ba4607cbede7ca091f990a109d0d99a3ec81
Barthy B <barthy.bonhomme@gmail.com>
2016-09-28 16:36:19 +0200

    ccindu.sh update

--- ccindu.sh
+++ ccindu.sh
@@ -1,1 +1,1 @@
-php5.5 app/console cache:clear --env=prod && php5.5 app/console assets:install --symlink /homepages/8/d567573230/htdocs/stage/MWB/web/ --env=prod && php5.5 app/console assetic:dump --env=prod /homepages/8/d567573230/htdocs/stage/MWB/web/
+php5.5 app/console cache:clear --env=prod && php5.5 app/console assets:install --symlink /homepages/8/d567573230/htdocs/stage/keinegrenzen/web/ --env=prod && php5.5 app/console assetic:dump --env=prod /homepages/8/d567573230/htdocs/stage/keinegrenzen/web/
