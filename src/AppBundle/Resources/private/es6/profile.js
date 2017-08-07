164a567fa472f135720097c85ec887d74974b1a2
Barthy B <barthy.bonhomme@gmail.com>
2017-08-07 17:25:58 +0200

    ga
    Took 19 minutes

--- src/AppBundle/Resources/private/es6/profile.js
+++ src/AppBundle/Resources/private/es6/profile.js
@@ -14,7 +14,7 @@ $(document).ready(() => {
             "<div>"
             + "<p class='text-big'>" + text + "</p>"
             + "<a class='btn btn-dialog btn-primary m-2' target='_blank' href='" + downloadUrl + "'>" + download + "</a>"
-            + "<a class='btn btn-dialog btn-success m-2' target='_blank' href='https://www.keinegrenzen.org/donate'>" + donate + "</a>"
+            + "<a class='btn btn-dialog btn-success m-2 donate-external' target='_blank' href='https://www.keinegrenzen.org/donate'>" + donate + "</a>"
             + "</div>";
 
         e.preventDefault();
