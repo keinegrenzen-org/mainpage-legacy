666c469b52967225b906b866bb460c3f2b038b28
Barthy B <barthy.bonhomme@gmail.com>
2017-09-15 04:36:19 +0200

    analaytics fix
    Took 8 minutes

--- src/AppBundle/Resources/private/es6/profile.js
+++ src/AppBundle/Resources/private/es6/profile.js
@@ -13,7 +13,7 @@ $(document).ready(() => {
         const content =
             "<div>"
             + "<p class='text-big'>" + text + "</p>"
-            + "<a class='btn btn-dialog btn-primary m-2' target='_blank' href='" + downloadUrl + "'>" + download + "</a>"
+            + "<a class='btn btn-dialog btn-primary m-2 download-album' target='_blank' href='" + downloadUrl + "'>" + download + "</a>"
             + "<a class='btn btn-dialog btn-success m-2 donate-external' target='_blank' href='https://www.keinegrenzen.org/donate'>" + donate + "</a>"
             + "</div>";
 
