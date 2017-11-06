b17396e0b5287084568247b45db38119e346890f
114135 <u80234622@cbi-bs1.server.lan>
2017-11-06 22:38:20 +0100

    2 albums, images, script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-php5.5 bin/console cache:clear --env=prod && php5.5 bin/console assets:install --symlink /homepages/8/d567573230/htdocs/keinegrenzen/web/ --env=prod && php5.5 bin/console assetic:dump --env=prod /homepages/8/d567573230/htdocs/keinegrenzen/web/
+php7.1 bin/console cache:clear --env=prod && php7.1 bin/console assets:install --symlink /homepages/8/d567573230/htdocs/keinegrenzen/web/ --env=prod && php7.1 bin/console assetic:dump --env=prod /homepages/8/d567573230/htdocs/keinegrenzen/web/
