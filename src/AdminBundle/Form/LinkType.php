8a371dd4ae28644c578c76c54425b3edded1159e
BarthyB <barthy.bonhomme@gmail.com>
2018-09-22 14:31:59 +0200

    luxus
    Took 3 minutes

---
+++ src/AdminBundle/Form/LinkType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LinkType extends AbstractType {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('link', UrlType::class, array(
+                'label' => 'link.label.url',
+                'required' => true
+            ));
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'AdminBundle\Entity\Link'
+        ));
+    }
+}
